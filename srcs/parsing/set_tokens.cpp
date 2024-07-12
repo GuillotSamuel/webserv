460e5264dd38e38be60008c80694af22b75f8ad2
GuillotSamuel <samuelguillot75@gmail.com>
2024-07-12 19:04:46 +0200

    correction : set_tokens

--- srcs/parsing/set_tokens.cpp
+++ srcs/parsing/set_tokens.cpp
@@ -6,7 +6,7 @@
 /*   By: sguillot <sguillot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 16:09:51 by sguillot          #+#    #+#             */
-/*   Updated: 2024/07/12 18:51:33 by sguillot         ###   ########.fr       */
+/*   Updated: 2024/07/12 19:04:12 by sguillot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,41 +81,52 @@ static bool ft_set_listen_param(t_data *data, std::vector<std::string> tokens)
     return (true);
 }
 
-static bool ft_is_ip_address(const std::string& str) {
+static bool ft_is_ip_address(const std::string& str)
+{
     if (str.size() < 7 || str.size() > 15
         || std::strncmp("10", str.c_str(), 2) == 0
         || std::strncmp("192.168", str.c_str(), 7) == 0
         || std::strncmp("0.0.0.0", str.c_str(), 7) == 0
-        || std::strncmp("255.255.255.255", str.c_str(), 15) == 0) {
-        return false;
+        || std::strncmp("255.255.255.255", str.c_str(), 15) == 0)
+    {
+        return (false);
     }
 
     int num = 0;
     int point_counter = 0;
     int segment_length = 0;
 
-    for (size_t i = 0; i < str.size(); ++i) {
+    for (size_t i = 0; i < str.size(); ++i)
+    {
         char ch = str[i];
-        if (ch == '.') {
-            if (num < 0 || num > 255 || segment_length == 0 || segment_length > 3) {
-                return false;
+        if (ch == '.')
+        {
+            if (num < 0 || num > 255 || segment_length == 0 || segment_length > 3)
+            {
+                return (false);
             }
             num = 0;
             segment_length = 0;
             ++point_counter;
-        } else if (ch >= '0' && ch <= '9') {
+        }
+        else if (ch >= '0' && ch <= '9')
+        {
             num = num * 10 + (ch - '0');
             ++segment_length;
-        } else {
-            return false;
+        }
+        else
+        {
+            return (false);
         }
     }
 
-    if (num < 0 || num > 255 || point_counter != 3 || segment_length == 0 || segment_length > 3) {
-        return false;
+    if (num < 0 || num > 255 || point_counter != 3
+        || segment_length == 0 || segment_length > 3)
+    {
+        return (false);
     }
 
-    return true;
+    return (true);
 }
 
 
