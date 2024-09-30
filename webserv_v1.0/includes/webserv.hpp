b9de67aa89d01f36d69410f38a23eeff677666c7
Samuel Guillot <sguillot@paul-f4Ar8s1.clusters.42paris.fr>
2024-09-30 18:58:14 +0200

    correction : HELP_INFOS

--- webserv_v1.0/includes/webserv.hpp
+++ webserv_v1.0/includes/webserv.hpp
@@ -6,7 +6,7 @@
 /*   By: sguillot <sguillot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 13:51:56 by sguillot          #+#    #+#             */
-/*   Updated: 2024/09/30 17:20:46 by sguillot         ###   ########.fr       */
+/*   Updated: 2024/09/30 18:57:56 by sguillot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,15 +64,27 @@
 #define BLUE "\033[34m"
 #define RED "\033[31m"
 #define BOLD "\033[1m"
-#define HELP_INFOS "usage : ./webserv [<file.conf] [--help]\n\n" \
-    "informations required in the configuration file (token must be separated by spaces or tabulations):\n\n" \
+#define HELP_INFOS "\nUsage : ./webserv [<file.conf] [--help]\n\n" \
+    "Informations required in the configuration file (token must be separated by spaces or tabulations):\n\n" \
     "server {\n" \
-    "\tlisten <port number>\n\n" \
-    "\tserver_name <server name>\n\n" \
-    "\t error_page <error code> <error html file>\n\n" \
-    "\t client_max_body_size <size in M>\n\n" \
-    "\tlocation /<page name> {\n\t\tallow_methods <methods allowed>\n\t\tautoindex <on/off>\n\t}\n" \
-    "}"
+    "\tlisten <port number>\n" \
+    "\tserver_name <server name>\n" \
+    "\terror_page <error code> <error html file>\n" \
+    "\tclient_max_body_size <size in M>\n" \
+    "\tindex <index file path>\n" \
+    "\troot <root path>\n" \
+    "\tallow_methods <on/off>\n" \
+    "\tlocation <option: \"=\"> </page_name>\n\t\t" \
+    "| allow_methods <methods allowed>\n\t\t| auto_index <on/off>\n\t\t" \
+    "| alias <str alias>\n\t\t| root <str root> \n\t\t" \
+    "| client_max_body_size <int size>\n\t\t| index <str index file path>\n\t\t" \
+    "| error_page <int error code> <str error file path>\n\t\t| return <int code> <str return file path>" \
+    "| cgi <str .extension> <str cgi path>\n" \
+    "}\n" \
+    "\nOther instructions :" \
+    "\n\t- You can create multiple servers with the following tokens : server { }" \
+    "\n\t- Location informations must be written in the same lime separated by pipes\n"
+    
 
 extern int g_signal;
 
